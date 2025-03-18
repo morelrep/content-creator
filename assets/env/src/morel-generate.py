5f88763dd77c7c4a903ef90a8ec0f30541ac32b2
febr3s <eduardofebresm@gmail.com>
2025-03-18 10:19:52 -0400

    emptied folders to test

--- assets/env/src/morel-generate.py
+++ assets/env/src/morel-generate.py
@@ -1,13 +1,14 @@
 # import sys
 # sys.path.append('scripts/')
 
-import field_updates # pre-process
-import replace_quotes # pre-process
-import copy_img # content-collections
 # import copy_about # website-content
+
+import field_updates # csv-process
+import replace_quotes # csv-process
+import copy_img # content-collections
 import csv_to_md_pub # content-collections
 import csv_to_md_auth # content-collections
 import csv_to_md_books # content-collections
 import csv_to_md_cities # content-collections
 import csv_to_md_repo # content-collections
-import blank_column # pre-process
+import blank_column # csv-process
